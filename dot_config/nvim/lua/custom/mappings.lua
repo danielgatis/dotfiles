1f7ee0dd23340a7fe942ac89cf6ec35f67ca0cb6
Daniel Gatis <danielgatis@gmail.com>
2023-06-09 01:24:56 -0300

    x

--- dot_config/nvim/lua/custom/mappings.lua
+++ dot_config/nvim/lua/custom/mappings.lua
@@ -4,6 +4,16 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<M-l>"] = { function() require('Navigator').right() end, "Navigate tmux right", opts = { nowait = true } },
+    ["<M-h>"] = { function() require('Navigator').left() end, "Navigate tmux left", opts = { nowait = true } },
+    ["<M-k>"] = { function() require('Navigator').up() end, "Navigate tmux up", opts = { nowait = true } },
+    ["<M-j>"] = { function() require('Navigator').down() end, "Navigate tmux down", opts = { nowait = true } },
+  },
+  t = {
+    ["<M-l>"] = { function() require('Navigator').right() end, "Navigate tmux right", opts = { nowait = true } },
+    ["<M-h>"] = { function() require('Navigator').left() end, "Navigate tmux left", opts = { nowait = true } },
+    ["<M-k>"] = { function() require('Navigator').up() end, "Navigate tmux up", opts = { nowait = true } },
+    ["<M-j>"] = { function() require('Navigator').down() end, "Navigate tmux down", opts = { nowait = true } },
   },
 }
 
