663141cfe4d739e2cdb39b5912ca64398647ddc7
Daniel Gatis <danielgatis@gmail.com>
2024-02-08 00:13:22 -0300

    x

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -14,23 +14,6 @@ vim.opt.signcolumn = "yes"
 vim.opt.showmode = false
 vim.opt.termguicolors = true
 
--- Keymaps
-vim.keymap.set("n", "<c-p>", "<cmd>Telescope find_files hidden=true no_ignore=true<cr>")
-vim.keymap.set("n", "<c-s-p>", "<cmd>Telescope live_grep<cr>")
-vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFindFileToggle<cr>")
-vim.keymap.set("n", "<leader>dd", function() require("duck").hatch() end, {})
-vim.keymap.set("n", "<leader>dk", function() require("duck").cook() end, {})
-vim.keymap.set("n", "<leader>S", '<cmd>lua require("spectre").toggle()<cr>')
-vim.keymap.set("n", "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<cr>')
-vim.keymap.set("v", "<leader>sw", '<esc><cmd>lua require("spectre").open_visual()<cr>')
-vim.keymap.set("n", "<leader>sp", '<cmd>lua require("spectre").open_file_search({select_word=true})<cr>')
-vim.keymap.set("n", "<m-tab>", "<cmd>BufferLineCycleNext<cr>")
-vim.keymap.set("n", "<m-s-tab>", "<cmd>BufferLineCyclePrev<cr>")
-vim.keymap.set("n", "<m-q>", '<cmd>lua require("bufdelete").bufdelete(0, true)<cr>')
-vim.keymap.set("n", "<leader>gb", '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>', { silent = true })
-vim.keymap.set("v", "<leader>gb", '<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr>', { silent = true })
-vim.keymap.set("i", "<c-cr>", 'copilot#Accept("\\<cr>")', { expr = true, replace_keycodes = false })
-
 -- Global Variables
 vim.g.mapleader = " "
 vim.g.copilot_no_tab_map = true
@@ -38,6 +21,22 @@ vim.g.lsp_zero_extend_cmp = 0
 vim.g.lsp_zero_extend_lspconfig = 0
 vim.g.blamer_enabled = 1
 vim.g.blamer_show_in_insert_modes = 0
+
+-- Keymaps
+vim.keymap.set("n", "<c-p>", "<cmd>lua require('fzf-lua').files()<cr>", { silent = true })
+vim.keymap.set("n", "<c-s-p>", "<cmd>lua require('fzf-lua').live_grep()<cr>", { silent = true })
+vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>")
+vim.keymap.set("n", "<leader>f", "<cmd>NvimTreeFindFile<cr>")
+vim.keymap.set("n", "<leader>dd", function() require("duck").hatch() end)
+vim.keymap.set("n", "<leader>dk", function() require("duck").cook() end)
+vim.keymap.set("n", "<m-tab>", "<cmd>BufferLineCycleNext<cr>")
+vim.keymap.set("n", "<m-s-tab>", "<cmd>BufferLineCyclePrev<cr>")
+vim.keymap.set("n", "<m-q>", '<cmd>lua require("bufdelete").bufdelete(0, true)<cr>')
+vim.keymap.set("n", "<leader>gb", '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>', { silent = true })
+vim.keymap.set("v", "<leader>gb", '<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr>', { silent = true })
+vim.keymap.set("i", "<c-cr>", 'copilot#Accept("\\<cr>")', { expr = true, replace_keycodes = false })
+vim.keymap.set("n", "tt", "<cmd>:Other<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { noremap = true, silent = true })
 
 -- Plugins
 
@@ -56,26 +55,31 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   { "akinsho/bufferline.nvim" },
+  { "APZelos/blamer.nvim" },
   { "famiu/bufdelete.nvim" },
   { "folke/tokyonight.nvim" },
   { "folke/trouble.nvim" },
+  { "github/copilot.vim" },
   { "hrsh7th/cmp-buffer" },
   { "hrsh7th/cmp-nvim-lsp" },
   { "hrsh7th/cmp-path" },
   { "hrsh7th/nvim-cmp", event = "InsertEnter" },
+  { "ibhagwan/fzf-lua" },
+  { "L3MON4D3/LuaSnip", version = "v2.*", build = "make install_jsregexp" },
+  { "mfussenegger/nvim-lint" },
   { "mg979/vim-visual-multi", branch = "master" },
   { "neovim/nvim-lspconfig", cmd = { "LspInfo", "LspInstall", "LspStart" }, event = { "BufReadPre", "BufNewFile" } },
   { "numToStr/Comment.nvim" },
   { "nvim-lua/plenary.nvim" },
   { "nvim-lualine/lualine.nvim" },
-  { "nvim-pack/nvim-spectre" },
-  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-  { "nvim-telescope/telescope.nvim", branch = "0.1.x" },
   { "nvim-tree/nvim-tree.lua" },
   { "nvim-tree/nvim-web-devicons" },
   { "nvim-treesitter/nvim-treesitter-textobjects" },
   { "nvim-treesitter/nvim-treesitter" },
+  { "rgroli/other.nvim" },
   { "RRethy/nvim-treesitter-endwise" },
+  { "ruifm/gitlinker.nvim" },
+  { "stevearc/conform.nvim" },
   { "sunaku/tmux-navigate", lazy = false },
   { "tamton-aquib/duck.nvim" },
   { "tpope/vim-repeat" },
@@ -84,12 +88,6 @@ require("lazy").setup({
   { "wellle/targets.vim" },
   { "williamboman/mason-lspconfig.nvim" },
   { "williamboman/mason.nvim", lazy = false, config = true },
-  { "github/copilot.vim" },
-  { "APZelos/blamer.nvim" },
-  { "L3MON4D3/LuaSnip", version = "v2.*", build = "make install_jsregexp" },
-  { "mfussenegger/nvim-lint" },
-  { "ruifm/gitlinker.nvim" },
-  { "stevearc/conform.nvim" },
 }, {
   install = {
     colorscheme = {
@@ -100,6 +98,35 @@ require("lazy").setup({
 
 -- tokyonight
 vim.cmd("colorscheme tokyonight-night")
+
+-- other
+require("other-nvim").setup({
+  mappings = {
+    { pattern = "(.*).ts$", target = "%1.test.ts" },
+    { pattern = "(.*).test.ts$", target = "%1.ts" },
+    { pattern = "(.*).tsx$", target = "%1.test.tsx" },
+    { pattern = "(.*).test.tsx$", target = "%1.tsx" },
+    { pattern = "(.*).js$", target = "%1.test.js" },
+    { pattern = "(.*).test.js$", target = "%1.js" },
+    { pattern = "(.*).jsx$", target = "%1.test.jsx" },
+    { pattern = "(.*).test.jsx$", target = "%1.jsx" },
+    { pattern = "(.*).go$", target = "%1_test.go" },
+    { pattern = "(.*)_test.go$", target = "%1.go" },
+    { pattern = "app/(.*).rb$", target = "spec/%1_spec.rb" },
+    { pattern = "spec/(.*)_spec.rb$", target = "app/%1.rb" },
+  },
+  hooks = {
+    onFindOtherFiles = function(matches)
+      local filtered_matches = {}
+      for _, match in ipairs(matches) do
+        if not string.match(match.filename, ".test.test.") then
+          table.insert(filtered_matches, match)
+        end
+      end
+      return filtered_matches
+		end,
+  }
+})
 
 -- conform
 require("conform").setup({
@@ -156,14 +183,10 @@ require("nvim-tree").setup({})
 -- comment
 require("Comment").setup({})
 
--- telescope
-local telescope = require("telescope")
-telescope.setup({
-  defaults = {
-    preview = false,
-  },
+-- fzf-lua
+require("fzf-lua").setup({
+  file_ignore_patterns = { 'node_modules/.*', 'vendor/.*', 'tmp/.*', '.git/.*'},
 })
-telescope.load_extension("fzf")
 
 -- endwise
 require("nvim-treesitter.configs").setup({
